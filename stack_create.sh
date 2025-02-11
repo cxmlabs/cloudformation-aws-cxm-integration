45e597992eb15c9d9fbccff9401a307d576d856d
Ronan Delacroix <ronan.delacroix@gmail.com>
2025-02-11 23:30:39 +0100

    feat: initial commit - working version

---
+++ stack_create.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ORG_UNIT="r-abcd"
+
+# root stack
+aws cloudformation create-stack --stack-name CxmIntegrationStack-Main \
+  --template-body file://cxm-integration-aws-root.yaml \
+  --parameters file://params-cxm-root-example.json \
+  --capabilities CAPABILITY_NAMED_IAM
+
+# sub accounts stack-set
+aws cloudformation create-stack-set \
+  --stack-set-name CxmIntegrationStack-SubAccounts \
+  --template-body file://cxm-integration-aws-sub-account.yaml \
+  --parameters file://params-cxm-sub-accounts-example.json \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --permission-model SERVICE_MANAGED \
+  --auto-deployment "Enabled=true,RetainStacksOnAccountRemoval=false"
+
+aws cloudformation create-stack-instances \
+  --stack-set-name CxmIntegrationStack-SubAccounts \
+  --deployment-targets "OrganizationalUnitIds=${ORG_UNIT}" \
+  --operation-preferences "FailureToleranceCount=0,MaxConcurrentCount=5" \
+  --regions "us-east-1" "us-east-2" "us-west-1" "us-west-2" "eu-west-1" "eu-west-2" "eu-west-3"
